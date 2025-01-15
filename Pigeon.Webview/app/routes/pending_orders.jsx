918ab663c14bedeee87eb87999c3cef774bd5254
TanmayArya <72544600+TanmayArya-1p@users.noreply.github.com>
2025-01-15 14:43:13 +0530

    routing

--- Pigeon.Webview/app/routes/pending_orders.jsx
+++ Pigeon.Webview/app/routes/pending_orders.jsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router";
+import PendingOrdersList from "../components/pendingOrders";
 
 export default function PendingOrders() {
   return (
-    <div>
-      <h1>Dashboard</h1>
-      {/* will either be home.tsx or settings.tsx */}
-      <Outlet />
+    <div className="flex-1 w-full h-full mb-20">
+      <div className="flex flex-col items-start w-[75%] ml-[20%] mt-[10%] ">
+          <div className="text-4xl mb-6">Pending Orders</div>
+          <PendingOrdersList limit={-1} />
+      </div>
+      <div className="h-10"></div>
     </div>
   );
 }
